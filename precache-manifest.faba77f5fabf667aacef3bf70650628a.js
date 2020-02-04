7c32c53e8516595e9c74e2b3fe23cf7a20532ed0
Amit Varghese <amitvarghese2011@gmail.com>
2020-02-05 01:20:28 +0530

    Updates

---
+++ precache-manifest.faba77f5fabf667aacef3bf70650628a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7df646477f5d8ae99e30de763888e3b5",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "22f39b57fc2c3dfef7b4",
+    "url": "/calculator/static/css/main.4c94ab45.chunk.css"
+  },
+  {
+    "revision": "995cb08f7f85acae095d",
+    "url": "/calculator/static/js/2.b36cd9b3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/calculator/static/js/2.b36cd9b3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22f39b57fc2c3dfef7b4",
+    "url": "/calculator/static/js/main.4d18dc7e.chunk.js"
+  },
+  {
+    "revision": "28828353795638a97109",
+    "url": "/calculator/static/js/runtime-main.63ccf6e2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
